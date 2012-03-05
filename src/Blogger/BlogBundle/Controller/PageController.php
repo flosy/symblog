0c4d35f22defeda0364ed68c1455bacf4fef9868
Flosy <contact@flosy.info>
2012-03-05 23:50:29 +0100

    Add BlogBundle.

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+}
