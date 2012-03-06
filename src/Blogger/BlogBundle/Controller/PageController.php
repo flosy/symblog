365f7512fbaa81cbf8946e6267b7f6a8eb4fed59
Flosy <contact@flosy.info>
2012-03-06 13:51:07 +0100

    Adding about page

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -10,4 +10,9 @@ class PageController extends Controller
     {
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
     }
+    
+    public function aboutAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
 }
